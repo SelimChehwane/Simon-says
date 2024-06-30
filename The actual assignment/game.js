47044b5a355d1517e203ce702153d8358210af77
SelimChehwane <selim_chehwane@hotmail.com>
2024-06-30 19:07:51 +0300

    adding start button

--- The actual assignment/game.js
+++ The actual assignment/game.js
@@ -1,1 +1,12 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
 
+document.addEventListener("keypress", () => {
+    if (!started) {
+      alert("Start game")
+      started = true;
+    }
+  });
