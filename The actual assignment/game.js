24476d09691b63cd5b036e476ba1d73cffb6abbf
SelimChehwane <selim_chehwane@hotmail.com>
2024-06-30 20:13:08 +0300

    adding checkAnswer function

--- The actual assignment/game.js
+++ The actual assignment/game.js
@@ -7,7 +7,9 @@ let level = 0;
 document.addEventListener("keypress", () => {
   if (!started) {
     document.querySelector("#level-title").textContent = `Level ${level}`;
-    started = true;
+    gameSequence()
+    ;started = true;
+    
   }
 });
 
@@ -17,29 +19,54 @@ document.querySelectorAll(".btn").forEach((btn) => {
     userClickedPattern.push(id);
     clickAnimation(id);
     playAudio(id);
-    gameSequence();
-    alert(gamePattern)
+    checkAnswer(userClickedPattern.length - 1);
   });
 });
 
 function clickAnimation(id) {
-    let element = document.getElementById(id);
-    element.classList.add("pressed");
-    setTimeout(() => {
-      element.classList.remove("pressed");
-    }, 100);
-  }
+  let element = document.getElementById(id);
+  element.classList.add("pressed");
+  setTimeout(() => {
+    element.classList.remove("pressed");
+  }, 100);
+}
 
-  function playAudio(color) {
-    let relPath = `sounds/${color}.mp3`;
-    let audio = new Audio(relPath);
-    audio.play();
-  }
-  function gameSequence() {
-    let rand = Math.floor(Math.random() * 4);
+function playAudio(color) {
+  let relPath = `sounds/${color}.mp3`;
+  let audio = new Audio(relPath);
+  audio.play();
+}
+function gameSequence() {
+    userClickedPattern=[];
+  let rand = Math.floor(Math.random() * 4);
   let color = buttonColors[rand];
   gamePattern.push(color);
   clickAnimation(color);
+}
 
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        gameSequence();
+      }, 1000);
+    }
+  } else {
+    document.getElementById("level-title").textContent = "Game Over";
+    document.body.classList.add("game-over");
+    setTimeout(() => {
+      document.body.classList.remove("game-over");
+    }, 200);
+
+    let audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+
+    startOver();
   }
-
+}
